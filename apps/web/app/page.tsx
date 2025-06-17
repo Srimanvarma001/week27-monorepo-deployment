f03746c14d7b5b64bdb662dab88c660d2155a472
sriman varma <srimannarayanavarmadunde@gmail.com>
2025-06-18 00:41:52 +0530

    added the docker files

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import { prismaClient } from "db/client";
 
 export default async function Home() {
-  const users = await prismaClient.user.findMany();
+  // const users = await prismaClient.user.findMany();
   return (
     <div>
-      {JSON.stringify(users)}
+      {/* {JSON.stringify(users)} */}
+
+      hi there
     </div>
   );
 }
